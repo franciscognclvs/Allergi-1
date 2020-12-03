de6537f09208cf1bfea7651dcd6eb5b3c07b303c
francisco goncalves <fgp1991@gmail.com>
2020-12-03 17:01:59 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_141934) do
+ActiveRecord::Schema.define(version: 2020_12_03_191052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 2020_12_03_141934) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["substance_id"], name: "index_allergies_on_substance_id"
     t.index ["user_id"], name: "index_allergies_on_user_id"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "compound_mixes", force: :cascade do |t|
