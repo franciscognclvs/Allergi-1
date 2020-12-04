8e370993395044c284a3b2eaecb47b693500253f
francisco goncalves <fgp1991@gmail.com>
2020-12-04 10:30:47 -0300

    seed de user e doctor para teste

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :substances, through: :allergies
   has_many :compound_mixes, through: :substances
   has_many :medicines, through: :compound_mixes
-  validates :name, :phone, :cpf, :birthdate, :weight, :address, :email, presence: true
+  validates :name, :phone, :cpf, :weight, :address, :email, presence: true
   has_one_attached :photo
   has_many :appointments
-  belongs_to :doctor
+  # belongs_to :doctor
 end
